7b83849a7ded4159eafbc083e513ed322b1edb50
zjwgank <zjwgank@appledeMBP.lan>
2019-11-02 21:44:36 +0800

    feat: module-Config add

---
+++ samples/module-config/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+    title: '模块引入配置',
+    description: 'module-config',
+    version: '1.0.0'
+}
---
+++ samples/module-config/index.ts
@@ -0,0 +1,8 @@
+import { logTool } from 'action/index';
+import config from 'config/index';
+
+const str: string = '模块引入配置'
+
+logTool()
+console.log(config)
+console.log(str)
